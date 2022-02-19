93a2c24a520a85ada029cd3e76c8226adabf1b59
zaki98D <ounissizakaria@gmail.com>
2022-02-19 15:02:50 +0100

    add help

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,56 @@ import backSpace from "./keyboard_backspace_black_24dp.svg"
 import returnKey from "./keyboard_return_black_24dp.svg"
 import './App.css'
 import wordsFile from "./5-letter-ar-words.txt"
-
+import example1 from "./example-1.png"
 const horof = "ّضصثقفغعهخحجدشسيبلاتنمكطذئءؤرىةوزظ".split("")
-function Header() {
+function Header({ helpModal, setHelpModal }) {
+  let isHelp = helpModal ? "" : "hidden"
   return (
     <header className='flex w-screen px-6 py-2 shadow-md h-14'>
       <h1 className='m-auto font-mono text-4xl font-black'>
         كلمات
       </h1>
-      <button className='h-full'>
+      <button className='h-full' onClick={() => setHelpModal(true)}>
         <img src={help} alt="مساعدة" title='مساعدة' />
       </button>
+      <div className={'fixed inset-0 grid bg-black/30 place-items-center ' + isHelp}>
+        <div className='absolute px-4 py-8 m-auto space-y-4 bg-white border rounded border-stone-100'>
+          <h2 className='text-2xl font-bold text-center'>
+            كيفية اللعب
+          </h2>
+          <p>
+            خمن الكلمة في ست محاولات.
+          </p>
+          <p>
+            كل كلمة تدخلها يجب أن تكون كلمة صحيحة متكونة من 5 أحرف. اظغط على مفتاح العودة
+            ( &#11152; )
+            للموافقة على كلمتك.
+          </p>
+          <p>
+            بعد كل محاولة، سيتغير لون حروف الكلمة التي أدخلتها ليعكس مدى قربك للكلمة الصحيحة.
+          </p>
+          <p>
+            <span className='font-bold'>تذكير: </span>
+            تعتبر الشّدّة ( _ّ ) حرفا من الحروف.
+          </p>
+          <hr />
+          <h3 className='text-xl font-bold'>
+            مثال:
+          </h3>
+          <img src={example1} alt="الكلمة أهلّت، الشدة ملونة بالأخضر و ت ملونة بالأصفر." />
+
+          <p>
+            الحروف ا، ه، ل ليسوا جزءا من الكلمة الصحيحة.
+          </p>
+          <p>
+            الشّدّة جزء من الكلمة الصحيحة و هي في الموضع الصحيح.
+          </p>
+          <p>
+            الـ ت جزء من الكلمة الصحيحة، لكنها ليست في الموضع الصحيح.
+          </p>
+          <button className='absolute top-0 text-lg font-bold ' onClick={() => setHelpModal(false)}>x</button>
+        </div>
+      </div>
     </header>)
 }
 
@@ -138,7 +177,7 @@ function Main() {
     , [])
 
   useEffect(() => {
-    setTargetWord(wordsList[0])
+    setTargetWord(wordsList[4])
   })
   return (
     <main className='grid gap-4 my-4 justify-items-center place-items-center'>
@@ -220,10 +259,10 @@ function handleSubmit(event, currentWord, wordsList,
 }
 
 function App() {
-
+  const [helpModal, setHelpModal] = useState(false)
   return (
     <>
-      <Header></Header>
+      <Header helpModal={helpModal} setHelpModal={setHelpModal}></Header>
       <Main></Main>
     </>
   )
